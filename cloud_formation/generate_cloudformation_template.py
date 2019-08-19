56c73f215ac96e74a66fbdcabb553f4bba3e08fa
Oleh_Karpov <Oleh_Karpov@epam.com>
2019-08-19 15:29:36 +0300

    Cloudformation template generator

---
+++ cloud_formation/generate_cloudformation_template.py
@@ -0,0 +1,29 @@
+from troposphere import Template
+
+import aws_lambda
+import bucket
+import db
+import sns
+
+t = Template()
+
+t.add_resource(aws_lambda.lambda_processing)
+t.add_resource(aws_lambda.s3_lambda_permission)
+t.add_resource(aws_lambda.lambda_execution_role)
+t.add_parameter(aws_lambda.source_code_bucket)
+t.add_parameter(aws_lambda.zip_file_path)
+t.add_parameter(aws_lambda.lambda_name)
+
+t.add_resource(bucket.bucket)
+
+t.add_resource(db.db)
+t.add_parameter(db.db_user)
+t.add_parameter(db.db_password)
+t.add_parameter(db.db_name)
+t.add_resource(db.dynamo_db)
+
+t.add_resource(sns.sns_topic)
+t.add_resource(sns.subscription)
+
+with open('templates/lambda.json', 'w+') as f:
+    f.write(t.to_json())
---
+++ cloud_formation/sns.py
@@ -0,0 +1,13 @@
+
+from troposphere import (
+    Ref,
+    sns,
+)
+
+sns_topic = sns.Topic("SNSTopic", )
+subscription = sns.SubscriptionResource(
+    'SNSTopicSubscription',
+    Protocol='email',
+    Endpoint='bogdan.gm24@gmail.com',
+    TopicArn=Ref(sns_topic)
+)
