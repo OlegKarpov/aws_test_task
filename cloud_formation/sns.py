80ea636bae6131934c4cce03ce7d81f1ec4fc034
Oleh_Karpov <Oleh_Karpov@epam.com>
2019-08-20 11:57:20 +0300

    SNS email has been changed

--- cloud_formation/sns.py
+++ cloud_formation/sns.py
@@ -7,6 +7,6 @@ sns_topic = sns.Topic("SNSTopic", )
 subscription = sns.SubscriptionResource(
     'SNSTopicSubscription',
     Protocol='email',
-    Endpoint='bogdan.gm24@gmail.com',
+    Endpoint='karpovhub@gmail.com',
     TopicArn=Ref(sns_topic)
 )
